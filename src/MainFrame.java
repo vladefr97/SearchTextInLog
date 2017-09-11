bb3e3c52231a2021c57d7371ac0929e19d182cd2
LizaLizavetka <31848513+LizaLizavetka@users.noreply.github.com>
2017-09-11 22:25:46 +0300

    главное окошко
    плюс текстовая область

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,11 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 
 public class MainFrame extends JFrame {
+	 private JTextArea theText;
+	 
 	MainFrame (){
 		super ("SearchStringInLog");
-		 setBounds(100, 100, 200, 200); 
-         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		 setBounds(100, 100, 700, 400); 
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         initMainPanel();
+	}
+	
+	private void initMainPanel(){
+		JPanel panel = new JPanel(new BorderLayout());
+	    theText = new JTextArea();
+	    panel.add(new JScrollPane(theText), BorderLayout.NORTH);
+		getContentPane().add(panel);
+		
 	}
 	
 }
--- src/RunSearch.java
+++ src/RunSearch.java
@@ -2,7 +2,7 @@
 public class RunSearch {
 
 	public static void main(String[] args) {
-		System.out.println ("Говнокодь");
+		//System.out.println ("Говнокодь");
 		
 		MainFrame mainFrame = new MainFrame();
 		mainFrame.setVisible(true);
