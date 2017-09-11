acb5a17c054734a235550ceaf2db3f8915d7df15
LizaLizavetka <31848513+LizaLizavetka@users.noreply.github.com>
2017-09-11 21:55:04 +0300

    MainFrame
    морда..

---
+++ src/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+
+public class MainFrame extends JFrame {
+	MainFrame (){
+		super ("SearchStringInLog");
+		 setBounds(100, 100, 200, 200); 
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	}
+	
+}
---
+++ src/RunSearch.java
@@ -0,0 +1,12 @@
+
+public class RunSearch {
+
+	public static void main(String[] args) {
+		System.out.println ("Говнокодь");
+		
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
+
+	}
+
+}
